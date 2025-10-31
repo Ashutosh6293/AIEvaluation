9367b083bc8728a621207020ae7876b12098ef55
Ashutosh6293 <ashujha6202@gmail.com>
2025-10-31 14:04:17 +0530

    changes

--- frontendview/src/api.jsx
+++ frontendview/src/api.jsx
@@ -9,8 +9,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: "https://api-aieval.gspl.cloud", 
-  baseURL: "http://127.0.0.1:8000", 
+  baseURL: "https://api-aieval.gspl.cloud", 
+  // baseURL: "http://127.0.0.1:8000", 
 });
 
 // -------------------- Employee Evaluations --------------------
